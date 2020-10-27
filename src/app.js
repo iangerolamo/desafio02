06ec9aca3af45742a9c06fa8fea61e1fd801bb32
igerolamo <Kbbzkp53>
2020-10-27 17:57:39 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,112 @@
+const express = require("express");
+const cors = require("cors");
+
+const { v4 } = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+// function logRequests(request, response, next) {
+//   const { method, url } = request;
+
+//   const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+//   console.log(logLabel);
+
+//   return next(); // Próximo middleware
+// }
+
+// function validateProjectId(request, response, next) {
+//   const { id } = request.params;
+
+//   if (!validate(id)) {
+//     return response.status(400).json({ error: 'Invalid project ID.'})
+//   }
+
+//   return next();
+// }
+
+// app.use(logRequests);
+// app.use('/repositories/:id', validateProjectId);
+
+app.get("/repositories", (request, response) => {
+
+  return response.json(repositories);
+
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = { 
+    id: v4(), 
+    title, 
+    url, 
+    techs, 
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository)
+
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exist.' });
+  }
+  
+  repositories[repositoryIndex].likes++;
+
+  return response.json(repositories[repositoryIndex]);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exist.' });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex >= 0) {
+    repositories.splice(repositoryIndex, 1);
+  } else {
+    return response.status(400).json({ error: 'Repository does not exists.'});
+  }
+
+  return response.status(204).send();
+
+});
+
+module.exports = app;
